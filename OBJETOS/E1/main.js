f86649835458fb146883879048ea1f5c9508f554
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-04-03 20:07:19 +0200

    e1 terminado e2 ejecuciuon

---
+++ OBJETOS/E1/main.js
@@ -0,0 +1,37 @@
+// - Escribe una frase que diga "mi ordenador es un HP, con procesador i7 y 16Gb de RAM"
+
+// - Recorre todos los valores con un bucle for in
+
+// - Muestra el tercer extra del ordenador
+
+
+const ordenador1 = {
+
+    marca: "HP",
+    
+    procesador: "i7",
+    
+    ram: "16Gb",
+    
+    HD: "1Tb",
+    
+    precio: 1500,
+    
+    extras: ["cámara HD", "microfono stereo", "disco de acceso rápido"],
+    
+    maletin: true,
+    
+    }
+//FRASE
+    console.log("mi ordenador es un " +ordenador1.marca+ " , con procesador " +ordenador1.procesador+ " y "+ordenador1.ram+"de RAM");
+
+
+//TODO
+    for (let x in ordenador1){
+        console.log(x + ":");
+        console.log(ordenador1[x]);
+
+    }
+//PUNTUAL
+console.log(ordenador1.extras[2]);
+
---
+++ OBJETOS/E2/main.js
@@ -0,0 +1,14 @@
+// Sobre este objeto:
+// - Añadir una característica: oferta (Boolean) -C-
+// - Modificar una característica: descripción -U-
+// - Borrar una característica -D-
+// - Mostrar el objeto antes y después -R-
+// - Probar a mostrar solo valores (values) y solo propiedades (keys)
+
+const galletas = {
+    marca: "Artiach",
+    nombre: "Chiquilín",
+    descripción: "galletas de mantequilla",
+    precio: 2.75,
+    hayExistencias: true
+    };
