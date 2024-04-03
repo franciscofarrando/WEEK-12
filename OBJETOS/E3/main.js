fb57f1f2c6c7b755ae2fee42dc6b297f2d9d3e19
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-04-03 20:29:32 +0200

    objetos armados

--- OBJETOS/E3/main.js
+++ OBJETOS/E3/main.js
@@ -3,3 +3,27 @@
 // - Con ese array de objetos, mostrar por ejemplo, todos los modelos. (opción preguntar al usuario
 // qué quiere mostrar)
 
+const coche1 = {
+    marca: "Audi", 
+    modelo: "A8", 
+    potencia: "286 cv", 
+    precio: "$115.000",
+    stock: true
+}
+
+
+const coche2 = {
+    marca: "BMW", 
+    modelo: "M3", 
+    potencia: "375 cv", 
+    precio: "$124.000",
+    stock: true
+}
+
+const coche3 = {
+    marca: "Cupra", 
+    modelo: "Born", 
+    potencia: "231 cv", 
+    precio: "$38.000",
+    stock: false
+}
