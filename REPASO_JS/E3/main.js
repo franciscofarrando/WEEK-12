ac0b11971394be701a31518d4168645e105ff184
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-04-04 12:29:01 +0200

    E3 de repaso

---
+++ REPASO_JS/E3/main.js
@@ -0,0 +1,8 @@
+let dia =
+prompt("Que dia de la semana te llama la atencion?:");
+let tiempo = 
+prompt("Commo te gustaria que este el tiempo ese dia?:");
+
+
+console.log("El dia " + dia + " estara " + tiempo);
+
