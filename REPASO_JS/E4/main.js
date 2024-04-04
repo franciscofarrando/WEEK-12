c93b700fccd436aaf77d6580e6ba6e0aae5db2c1
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-04-04 12:35:48 +0200

    E4 terminado

---
+++ REPASO_JS/E4/main.js
@@ -0,0 +1,15 @@
+const platosItalianos = [
+    "Pasta Carbonara",
+    "Pizza Margarita",
+    "Risotto de Champiñones",
+    "Lasagna",
+    "Osso Buco",
+    "Bruschetta",
+    "Tiramisú"
+  ];
+
+  for (i=0; i<=platosItalianos.length ; i++) {
+  
+  document.write(platosItalianos[i] + "<br>") ;
+  }
+  
