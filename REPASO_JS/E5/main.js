76e6687882c7a90389976a9c32df1cd380d39f76
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-04-04 13:19:48 +0200

    E5 terminado!

--- REPASO_JS/E5/main.js
+++ REPASO_JS/E5/main.js
@@ -1,4 +1,4 @@
-const lenguajesDeProgramacion = [
+const lenguajes = [
     "Python",
     "Java",
     "JavaScript",
@@ -12,9 +12,19 @@ const lenguajesDeProgramacion = [
     "SQL"
   ];
 
-  let lenguajesNumeros = lenguajesDeProgramacion.length;
-  while(c)
+function mostrarLenguages(){
+  let consulta = false;
 
-  let consulta (i=0, i >=lenguajesDeProgramacion.length+1, i++){
-    prompt("numero de orden:");
-  }
+  while( consulta == false){
+    let indice = prompt("Hay un listado de 10 lenguaje, lee cada uno del 1 al "+ lenguajes.length + " con el 0 se sale de la consulta");
+    console.log(indice);
+    if (indice > 0 && indice <= lenguajes.length){
+      alert("El lenguaje elegido es " + lenguajes[indice - 1]);
+    }else if (indice == 0){
+    consulta = true;
+    }else{
+    alert("introduce un numero del 1 al " + lenguajes.length);  
+}
+}
+}
+mostrarLenguages()
